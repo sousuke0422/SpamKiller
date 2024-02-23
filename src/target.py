c97e2bbd8d268cd92d66fccb7733504347d920b1
sousuke0422 <sousuke20xx@gmail.com>
2024-02-23 19:32:50 +0900

    chore: add pattern
    support: https://kr.akirin.xyz/notes/818dc130f3a4efb3b7d5c42c

--- src/target.py
+++ src/target.py
@@ -6,6 +6,7 @@ from src.type import TTarget
 TARGETS: list[TTarget] = [
     {'key': 'ctkpaarr', 'count': 0, 'dry_run': False},
     {'key': '荒らし.com', 'count': 0, 'dry_run': False},
+    {'key': '画像が貼れなかったのでメンションだけします', 'count': 0, 'dry_run': False},
     # { 'key': 'xn--68j5e377y.com', 'count': 0, 'dry_run': False },
     {'key': 'test', 'count': 0, 'dry_run': True},
 ]
