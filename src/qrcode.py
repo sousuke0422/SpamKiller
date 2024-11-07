bfd4bd1d807074bd73a5889a5fddd5f7396fa5d7
yupix <yupi0982@outlook.jp>
2024-11-07 16:01:12 +0000

    feat: タイムライン上から消すためのコード(仮)を追加

--- src/qrcode.py
+++ src/qrcode.py
@@ -1,9 +1,24 @@
+import asyncio
 import os
+import sys
 
-import aiohttp
-from mipac.models.drive import File
 import cv2
+import aiohttp
+from loguru import logger
 from qreader import QReader
+from mipac.client import Client
+from mipac.http import Route
+from mipac.utils.pagination import Pagination
+from mipac.models.drive import File
+from mipac.models.note import Note
+from mipac.types.note import INote
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from src.env import URL, TOKEN
+from src.func import spam_action
+from src.target import TARGETS
 
 
 class QRCodeChecker:
@@ -36,3 +51,37 @@ class QRCodeChecker:
         await self.cleanup()
 
         return texts
+
+async def check_old_timeline():
+    try:
+        with open('tmp/last_id.txt', 'r') as f:
+            last_id = f.read()
+    except FileNotFoundError:
+        last_id = None
+    client = Client(URL, TOKEN)
+    pagination = Pagination[INote](client.http, Route('POST', '/api/notes/local-timeline'), json={
+        'untilId': last_id
+    })
+    
+    
+    while pagination.is_final is False:
+        raw_notes = await pagination.next()
+        for raw_note in raw_notes:
+            note = Note(raw_note, client=client.api)
+            
+            if 0 < len(note.mentions):
+                for file in note.files:
+                    texts = await QRCodeChecker(file).check()
+                    for target in TARGETS:
+                        if any(target['key'] in text for text in texts if isinstance(text, str)):
+                            logger.success(f'QRコードにパターン一致: {target['key']}')
+                            await spam_action(note, client.api, target)
+                            break
+        
+        with open('tmp/last_id.txt', 'w') as f:
+            f.write(raw_notes[-1]['id'])
+
+
+
+if __name__ == '__main__':
+    asyncio.run(check_old_timeline())
